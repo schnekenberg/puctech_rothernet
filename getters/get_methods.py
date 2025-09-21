59bd5ccab332cbf648abcaef39b12991d214e41f
grazielle.almeida <grazielle.almeida@ppro.com>
2025-09-21 09:21:50 -0300

    feat(main): Create method to return the order id

--- getters/get_methods.py
+++ getters/get_methods.py
@@ -1,9 +1,6 @@
 
-
-def get_cpf(message : str) -> int:
+# def get_cpf(message : str) -> int: 
     
-    for letter in message:
-        if isinstance(letter, int):
 
 def is_interaction_over(message : str) -> bool:
     message = message.lower().strip()
@@ -19,8 +16,22 @@ def has_interaction_started(message : str) -> bool:
     message = message.lower().strip()
     has_started = False
 
-    if "Bem vindo ao restaurante" in message:
+    if "bem-vindo ao restaurante" or "bem vindo ao restaurante" in message:
         has_started = True
-        
+
     return has_started
+
+def get_order_id(message: str) -> int | None:
+    message = message.lower().strip()
+    words = message.split()
+    
+    for i, word in enumerate(words):
+        if word == "id" and i + 1 < len(words):
+            next_word = words[i + 1]
+            if next_word.isdigit() and len(next_word) == 2:
+                return int(next_word)
+    return None
+
+
+
         

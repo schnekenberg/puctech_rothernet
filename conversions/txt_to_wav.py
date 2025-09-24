7c1e9908db2fb9747ecec0017437d4f1c0f3ec62
grazielle.almeida <grazielle.almeida@ppro.com>
2025-09-24 10:15:21 -0300

    Create method to convert from text to wav format

--- conversions/txt_to_wav.py
+++ conversions/txt_to_wav.py
@@ -1,1 +1,8 @@
 # conversions/txt_to_wav.py
+from TTS.api import TTS
+
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False, gpu=False)
+
+def text_to_wav_pt(text: str, output_path: str = "output.wav"):
+    tts.tts_to_file(text=text, file_path=output_path, speaker="female", language="pt")
+    return output_path

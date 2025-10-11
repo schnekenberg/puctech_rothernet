2b43b5ec5aaec2c0fc1d11830ab78740e9e7c61d
grazielle.almeida <grazielle.almeida@ppro.com>
2025-10-11 15:56:51 -0300

    Restore database connection

---
+++ init_db.py
@@ -0,0 +1,9 @@
+
+from database import SessionLocal, crud, engine, Base
+from database.models import Client, Order, Dish, OrderItem
+
+# This will create all tables in your Supabase database
+#disclaimer: don't run this script because the tables have already been created on supabase
+Base.metadata.create_all(bind=engine)
+
+print("Tables created successfully!")

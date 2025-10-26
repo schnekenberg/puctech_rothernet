2a2ed8a63dea433d9fbae5dab2cebf7509e0b2f5
guilherme schnekenberg <gschnekenberg@gmail.com>
2025-10-25 22:31:04 -0300

    atualização de imports
    Updated import paths to reflect new directory structure.

--- servidor/init_db.py
+++ servidor/init_db.py
@@ -1,6 +1,6 @@
 
-from database import SessionLocal, crud, engine, Base
-from database.models import Client, Order, Dish, OrderItem
+from servidor.database import SessionLocal, crud, engine, Base
+from servidor.database.models import Client, Order, Dish, OrderItem
 
 # This will create all tables in your Supabase database
 #disclaimer: don't run this script because the tables have already been created on supabase
